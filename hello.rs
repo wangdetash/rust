108a9b8e0b068714dc1debf8b83be3e6e71e9008
wangdetash <wangdetash@gmail.com>
2017-10-23 10:01:57 +0530

    program to print a string

---
+++ hello.rs
@@ -0,0 +1,4 @@
+//first program to display a string 
+fn main() {							//fn declares that main is a function
+    println!("Hello, world!This is my first rust project");     //indent with 4 spaces instead of 1 tab
+}								// ! declares that println is a macro instead of normal function
